ec7ebbd6a30daec3198fe828d50c243e86971324
luguoeryi <1099064949@qq.com>
2018-10-08 00:09:33 +0800

    '使用bundle-analyzer分析打包结果'

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -5,9 +5,12 @@ const webpack = require('webpack')
 const PurifyCSS = require('purifycss-webpack') // 去除废代码 css
 const HtmlWebpackInlineChunkPlugin = require('html-webpack-inline-chunk-plugin') // 将代码块插入html
 const CleanWebpackPlugin = require('clean-webpack-plugin') // 清除目录
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin // 打包资源map图
 
 module.exports = {
     plugins: [
+
+        new BundleAnalyzerPlugin(),
 
         new PurifyCSS({
             paths: glob.sync([
